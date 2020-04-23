87fb1253d0d072aeae5ed56bc4cbfb1758f2bfd1
kntejaswini38 <kntejaswiini38@gmail.com>
2020-04-23 17:33:05 +0530

    publish_webpage

---
+++ myfiles/scripts/main.js
@@ -0,0 +1,30 @@
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Webpage of Nature, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Webpage of Nature, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+
+
+
